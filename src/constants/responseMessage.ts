483f7ab2b5fd8aebe3dc538e77686f5008ae55b2
vikasdeshmukh63 <vikasdeshmukh63@gmail.com>
2024-09-14 21:01:15 +0530

    feat: Rate limiter added

--- src/constants/responseMessage.ts
+++ src/constants/responseMessage.ts
@@ -1,7 +1,8 @@
 export default {
-    SUCCESS:`The operation has been successfull`,
-    SOMETHING_WENT_WRONG:`Something went wrong`,
-    NOT_FOUND:(entity:string)=>{
+    SUCCESS: `The operation has been successfull`,
+    SOMETHING_WENT_WRONG: `Something went wrong`,
+    NOT_FOUND: (entity: string) => {
         return `${entity} not found`
-    }
+    },
+    TOO_MANY_REQUESTS: `Too many requests! Please try again after some time`
 }
--- src/router/apirouter.ts
+++ src/router/apirouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import apiController from '../controller/apiController'
+import rateLimit from '../midddleware/rateLimit'
 
 const router = Router()
 
-router.route('/self').get(apiController.self)
+router.route('/self').get(rateLimit,apiController.self)
 router.route('/health').get(apiController.health)
 
 export default router
